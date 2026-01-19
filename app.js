c61569902407cc95512e2b653b0a7a518644bc1c
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-01-19 16:09:27 +0300

    Setup express app with authentication

---
+++ app.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import express from 'express';
+import path from 'node:path';
+import auth from './lib/auth.js';
+const __dirname = import.meta.dirname;
+
+const app = express();
+
+// Settings
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Auth
+app.use(auth.appSession);
+app.use(auth.passport.session());
+app.use(auth.storeUserInLocalObject);
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', auth.login);
+app.get('/logout', auth.logout);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app - listening on port ${PORT}!`);
+});
