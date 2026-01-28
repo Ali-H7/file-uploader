898eaa84c1d8605dc42220024a9b63f73955849f
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-01-28 15:12:49 +0300

    Refactor files formatting helper

--- helpers/formatFileShare.js
+++ helpers/formatFileShare.js
@@ -1,16 +1,31 @@
 import formateDate from './formatDate.js';
 import prettyBytes from 'pretty-bytes';
 
-export default function formatFileShare(shareObject, url) {
+export default function formatFolderShare(shareObject, url) {
   const currentDate = new Date();
-  return {
-    ...shareObject,
-    validUntil: formateDate(shareObject.validUntil),
-    file: {
+  let key = shareObject.folder ? 'folder' : 'file';
+  let content;
+
+  if (shareObject.folder) {
+    const formattedFiles = shareObject.folder.files.map((file) => {
+      return {
+        ...file,
+        fileSize: prettyBytes(file.fileSize),
+        uploadDate: formateDate(file.uploadDate),
+      };
+    });
+    content = { ...shareObject.folder, files: formattedFiles };
+  } else {
+    content = {
       ...shareObject.file,
       fileSize: prettyBytes(shareObject.file.fileSize),
       uploadDate: formateDate(shareObject.file.uploadDate),
-    },
+    };
+  }
+  return {
+    ...shareObject,
+    validUntil: formateDate(shareObject.validUntil),
+    [key]: content,
     user: { ...shareObject.user, fullName: `${shareObject.user.firstName} ${shareObject.user.lastName}` },
     status: shareObject.validUntil > currentDate,
     url,
